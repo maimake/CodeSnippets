5451f3d85a37e55e9f98a89e209dcd79b2625ac0
mai <yshxinjian@gmail.com>
2015-05-20 18:25:07 +0800

    Imported user code snippets

---
+++ notification_post.m
@@ -0,0 +1,10 @@
+// Notification Post
+// notipost
+//
+// IDECodeSnippetCompletionPrefix: notipost
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 88656877-76DB-4961-A641-41A54389EB76
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+[[NSNotificationCenter defaultCenter] postNotificationName:<#(NSString *)#> object:<#(id)#>];
